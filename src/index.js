ccad23221f126840d02cacb8b6a86d573e689f38
ilya <ilya@ilyas-MBP.Dlink>
2018-09-22 23:49:35 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+
+  let arr = [];
+  let i = 5;
+
+  while (i < number) {
+
+    arr.push(i);
+    i = i * 5;
+  }
+
+  let result = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    result += Math.floor(number / arr[i]);
+  }
+
+  return result;
 }
